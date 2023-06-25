f7baa33050c650df980a87373a1942a620ca10ae
rajesh <kanugurajesh3@gmail.com>
2023-06-25 23:52:12 +0530

    adding all the data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,30 @@
-import React,{ useState } from "react";
+import React,{ useState,useEffect } from "react";
 
 function Header() {
     const [show,setshow] = useState(true)
+    const [windowsize,setWindow] = useState(false)
+
     const clicked = () => {
         const header = document.getElementById("head")
         header.classList.toggle("animate-pre")
         setshow(!show);
     }
+
+    useEffect(() => {
+        const handleResize = () => {
+          setWindow(window.innerWidth > 950);
+          console.log(windowsize)
+        };
+    
+        handleResize()
+        window.addEventListener("resize", handleResize);
+      }, [windowsize]);
+
   return (
     <>
         <nav>
-            <ul className="animate" id="head">
+            {!windowsize && 
+                <ul className="animate" id="head">
                 {show &&
                     <li onClick={clicked} id="menu">
                         <div></div>
@@ -38,6 +52,26 @@ function Header() {
                     </>
                 }
             </ul>
+            }
+            {windowsize && 
+                <ul>
+                    <li>
+                        <span>&#x00D8;1.</span> &lt;about&gt;
+                    </li>
+                    <li>
+                        <span>&#x00D8;2.</span> &lt;skills&gt;
+                    </li>
+                    <li>
+                        <span>&#x00D8;3.</span> &lt;portfolio&gt;
+                    </li>
+                    <li>
+                        <span>&#x00D8;4.</span> &lt;career&gt;
+                    </li>
+                    <li>
+                        <span>&#x00D8;5.</span> &lt;contact&gt;
+                    </li>
+                </ul>
+            }
         </nav>
     </>
   )
