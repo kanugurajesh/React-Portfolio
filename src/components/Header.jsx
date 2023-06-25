6b420dfa13f510f88ef1d1f0f3a462473cfbe49d
rajesh <kanugurajesh3@gmail.com>
2023-06-25 22:47:17 +0530

    adding all the data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,42 @@
+import React,{ useState } from "react";
+
 function Header() {
+    const header = document.getElementById("head")
+    const [show,setshow] = useState(true)
+    const clicked = () => {
+        header.classList.toggle("animate-pre")
+        setshow(!show);
+    }
   return (
     <>
         <nav>
-            <ul>
-                <li>
-                    <span>&#x00D8;1.</span> &lt;about&gt;
-                </li>
-                <li>
-                    <span>&#x00D8;2.</span> &lt;skills&gt;
-                </li>
-                <li>
-                    <span>&#x00D8;3.</span> &lt;portfolio&gt;
-                </li>
-                <li>
-                    <span>&#x00D8;4.</span> &lt;career&gt;
-                </li>
-                <li>
-                    <span>&#x00D8;5.</span> &lt;contact&gt;
-                </li>
+            <ul className="animate" id="head">
+                {show &&
+                    <li onClick={clicked} id="menu">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </li>
+                }
+                {!show &&
+                    <>
+                        <li onClick={clicked}>
+                            <span>&#x00D8;1.</span> &lt;about&gt;
+                        </li>
+                        <li onClick={clicked}>
+                            <span>&#x00D8;2.</span> &lt;skills&gt;
+                        </li>
+                        <li onClick={clicked}>
+                            <span>&#x00D8;3.</span> &lt;portfolio&gt;
+                        </li>
+                        <li onClick={clicked}>
+                            <span>&#x00D8;4.</span> &lt;career&gt;
+                        </li>
+                        <li onClick={clicked}>
+                            <span>&#x00D8;5.</span> &lt;contact&gt;
+                        </li>
+                    </>
+                }
             </ul>
         </nav>
     </>
