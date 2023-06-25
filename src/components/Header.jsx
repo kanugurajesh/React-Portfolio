670f08620a4d5630934d9d740daa8a1a8c045c64
rajesh <kanugurajesh3@gmail.com>
2023-06-25 15:59:56 +0530

    adding all the files

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+function Header() {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li>
+                    <span>&#x00D8;1.</span> &lt;about&gt;
+                </li>
+                <li>
+                    <span>&#x00D8;2.</span> &lt;skills&gt;
+                </li>
+                <li>
+                    <span>&#x00D8;3.</span> &lt;portfolio&gt;
+                </li>
+                <li>
+                    <span>&#x00D8;4.</span> &lt;career&gt;
+                </li>
+                <li>
+                    <span>&#x00D8;5.</span> &lt;contact&gt;
+                </li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default Header;
