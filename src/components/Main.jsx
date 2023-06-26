7fa21795a0a15d9313579c07583136dd6e220a12
rajesh <kanugurajesh3@gmail.com>
2023-06-26 19:26:13 +0530

    adding all the data

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+// import './App.css'
+import '../main.css'
+import Section1 from './Section1'
+
+function Main() {
+  return (
+    <>
+      <Section1 />
+    </>
+  )
+}
+
+export default Main
---
+++ src/components/Section1.jsx
@@ -0,0 +1,11 @@
+function Section1() {
+  return (
+    <section id="section1">
+      <div id="section1-div">
+        <img src="https://ik.imagekit.io/hbzknb1hm/albert-dera-ILip77SbmOE-unsplash.jpg?updatedAt=1687786294881" alt="" id="section1-img"/>
+      </div>
+    </section>
+  )
+}
+
+export default Section1;
