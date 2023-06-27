15777323acd1171f7360ba36d680e99e7c4c99df
rajesh <kanugurajesh3@gmail.com>
2023-06-27 07:41:15 +0530

    adding all the data

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -4,7 +4,7 @@ function Section1() {
       <div id="section1-div">
         <img src="https://ik.imagekit.io/hbzknb1hm/albert-dera-ILip77SbmOE-unsplash.jpg?updatedAt=1687786294881" alt="" id="section1-img"/>
       </div>
-      <h1>Hi,I'm <span>Rajesh <br /> Full Stack Web Developer</span> </h1>
+      <h1>Hi,I'm <span>Rajesh <br /> Full Stack <br /> Web Developer</span> </h1>
     </section>
   )
 }
