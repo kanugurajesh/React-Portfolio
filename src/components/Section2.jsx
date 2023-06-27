331794efc94786fba20602e5a7a8ff5703a084ed
rajesh <kanugurajesh3@gmail.com>
2023-06-27 22:05:41 +0530

    adding all the data

---
+++ src/components/Section2.jsx
@@ -0,0 +1,11 @@
+function Section2() {
+    <>
+        <h2>01. &lt;a little about me&gt;</h2>
+        <p></p>
+        <h4>Get 💩 done.Fast.</h4>
+        <p></p>
+        <h3>&lt;/a little about me&gt;</h3>
+    </>
+}
+
+export default Section2;
