3132daa3e43f798de1984f9abb99d0d927b38fde
rajesh <kanugurajesh3@gmail.com>
2023-06-27 20:02:25 +0530

    adding all the data

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,10 +1,17 @@
-function Section1() {
+function Section1({windowsize}) {
   return (
     <section id="section1">
       <div id="section1-div">
         <img src="https://ik.imagekit.io/hbzknb1hm/rajeshjpeg?updatedAt=1687835488940" alt="" id="section1-img"/>
       </div>
-      <h1>Hi,I'm <span>Rajesh <br /> Full-Stack <br /> Web Developer</span> </h1>
+      {windowsize &&
+        <h1>Hi,I'm <span>Rajesh <br /> Full-Stack <br /> Web Developer</span> </h1>
+      }
+
+      {!windowsize &&
+        <h1>Hi,I'm <span>Rajesh <br /> Full-Stack Web Developer</span> </h1>
+      }
+
       <p>I'm a full-stack web developer and a python developer,I love to bring your creativity and ideas to fully functional and scalable web applications.</p>
       <div id="icons">
         <ul>
