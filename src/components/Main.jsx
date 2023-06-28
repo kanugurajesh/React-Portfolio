67cd79643554a68a379117d5feb9bd97a2a90a35
rajesh <kanugurajesh3@gmail.com>
2023-06-29 00:10:07 +0530

    adding all the data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
 import Section5 from './Section5'
+import Section6 from './Section6'
 
 function Main() {
   return (
@@ -14,6 +15,7 @@ function Main() {
       <Section3 />
       <Section4 />
       <Section5 />
+      <Section6 />
     </>
   )
 }
--- src/components/Section5.jsx
+++ src/components/Section5.jsx
@@ -5,9 +5,9 @@ function Section5() {
         <section id="section5">
             <h1><span>Ø4.</span> &lt;my working career&gt;</h1>
             <div id="history">
-                <p><span>Web Developer </span>@ Upwork 2023 - Present</p>
-                <p><span>Web Developer </span>@ Upwork 2023 - Present</p>
-                <p><span>Web Developer </span>@ Upwork 2023 - Present</p>
+                <p><span>Full-Stack Web Developer </span>@ Upwork 2023 - Present</p>
+                <p><span>Full-Stack Web Developer </span>@ Freelancer 2023 - Present</p>
+                <p><span>Full-Stack Web Developer </span>@ Replit 2023 - Present</p>
             </div>
             <h3>&lt;/my working career&gt;</h3>
         </section>
