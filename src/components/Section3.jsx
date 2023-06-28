d1e2ca1cc569c948137cb01d43640c502fa68619
rajesh <kanugurajesh3@gmail.com>
2023-06-28 19:55:08 +0530

    adding all the data

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -1,39 +1,37 @@
 import '../css/section3.css'
 
-function SkillTest({ language, ProficiencyRating }) {
+  function SkillTest({ language, ProficiencyRating }) {
     const blueBoxes = ProficiencyRating;
     const blackBoxes = 5 - ProficiencyRating;
+
     return (
       <div className="language">
-            <h2>{language}</h2>
-            <ul>
-            {[...Array(blueBoxes)].map((_, index) => (
-            <>
-                <li key={`blue-${index}`} style={{ backgroundColor: 'blue',opacity:(index+1)*0.2}}></li>
-            </>
-            ))}
+        <h2>{language}</h2>
+        <ul>
+          {[...Array(blueBoxes)].map((_, index) => (
+            <li key={`${language}-blue-${index}`} style={{ backgroundColor: 'blue', opacity: (index + 1) * 0.2 }}></li>
+          ))}
 
-            {[...Array(blackBoxes)].map((_, index) => (
-            <>
-                <li key={`black-${index}`} style={{ backgroundColor: '#ccc'}}></li>
-            </>
-            ))}
+          {[...Array(blackBoxes)].map((_, index) => (
+            <li key={`${language}-black-${index}`} style={{ backgroundColor: '#ccc' }}></li>
+          ))}
         </ul>
       </div>
     );
   }
-  
+
+
   function Section3() {
     return (
       <section id="section3">
             <h1><span>Ø2.</span> &lt;my skills &gt;</h1>
         <div>
-            <SkillTest language={"Python"} ProficiencyRating={5} />
-            <SkillTest language={"Javascript"} ProficiencyRating={3} />
-            <SkillTest language={"React"} ProficiencyRating={4} />
-            <SkillTest language={"Html"} ProficiencyRating={4} />
-            <SkillTest language={"Css"} ProficiencyRating={4} />
-            <SkillTest language={"Azure"} ProficiencyRating={3} />
+            <SkillTest language={"Python"} ProficiencyRating={3} />
+            <SkillTest language={"Javascript"} ProficiencyRating={2} />
+            <SkillTest language={"React"} ProficiencyRating={3} />
+            <SkillTest language={"Html"} ProficiencyRating={5} />
+            <SkillTest language={"Css"} ProficiencyRating={5} />
+            <SkillTest language={"Azure"} ProficiencyRating={5} />
         </div>
             <h3>&lt;/my skills &gt;</h3>
       </section>
---
+++ src/components/Section4.jsx
@@ -0,0 +1,10 @@
+import '../css/section4.css'
+
+function Section4() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Section4;
