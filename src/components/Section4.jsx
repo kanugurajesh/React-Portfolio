11d64e3b2d7e5790703fa035df8af5a037672b2e
rajesh <kanugurajesh3@gmail.com>
2023-06-28 22:15:27 +0530

    adding all the data

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -2,7 +2,7 @@ import '../css/section4.css'
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
-function Box({github,link,tech}){
+function Box({name,github,link,tech}){
     const randomkey = uuidv4();
     return (
         <div className="project-box">
@@ -15,7 +15,7 @@ function Box({github,link,tech}){
                             <a href={link}><img src="https://ik.imagekit.io/hbzknb1hm/icons8-link.gif?updatedAt=1687962773580" alt="" /></a>
                         </div>
                     </div>
-                    <h3>Restaurant Menu</h3>
+                    <h3>{name}</h3>
                     <p>A demonstration of restaurant menu using react and api</p>
                     <div className="project-footer">
                         {tech.map((number) => {
@@ -33,8 +33,12 @@ function Section4() {
             <h1><span>Ø3.</span> &lt;my portfolio&gt;</h1>
             <p>A small gallery of recent projects chosen by me.I've done all of them alone and these projects are only part of my projects</p>
             <div id="grid">
-                <Box github="https://github.com/rajesh604/restaurant-app" link="https://restaurant-app-c7wi.vercel.app/" tech={["Javascript","Html","Css","React","meal api","hosting"]}/>
+                <Box name={"Restaurant Menu"} github="https://github.com/rajesh604/restaurant-app" link="https://restaurant-app-c7wi.vercel.app/" tech={["Javascript","Html","Css","React","meal api","hosting"]}/>
+                <Box name={"Restaurant App"} github="https://github.com/rajesh604/django_fullstack_project" link="http://rajeshkanugu.pythonanywhere.com/" tech={["django","Html","Css","Javascript","meal api","pythonanywhere"]}/>
+                <Box name={"Space Exploration"} github="https://github.com/rajesh604/space-tourism" link="https://space-tourism-l7gb.vercel.app/" tech={["Javascript","Html","Css"]}/>
+                <Box name={"Home Page"} github="https://github.com/rajesh604/website" link="https://rajesh604.github.io/website/" tech={["Javascript","Html","Css","Bootstrap"]}/>
             </div>
+            <h2>&lt;/my portfolio&gt;</h2>
         </div>
     )
 }
