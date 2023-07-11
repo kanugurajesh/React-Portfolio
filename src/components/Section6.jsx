74e6e710fb6402326c9a94db65f5e676bff0ebdb
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-11 23:10:34 +0530

    adding all the data

--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -5,7 +5,7 @@ function Section6() {
         <section id="section6">
             <h1><span>Ø5.</span>&lt;get in touch&gt;</h1>
             <p>My inbox is always open.Whether you have a question or project or just want to say hi, I'll try my best to get back to you!</p>
-            <a href="resume.pdf" download="rajesh-resume.pdf">My Resume</a>
+            <a href="./resume.pdf" download="rajesh-resume.pdf">My CV</a>
             <h3>&lt;/get in touch&gt;</h3>
         </section>
     )
