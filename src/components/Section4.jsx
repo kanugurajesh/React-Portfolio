b6ce75293f2a74967b4e70d42ef3974a2dcf4367
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-21 16:49:16 +0530

    adding all the data

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -34,8 +34,9 @@ function Section4() {
             <p>A small gallery of recent projects chosen by me.I've done all of them alone and these projects are only part of my projects</p>
             <div id="grid">
                 <Box name={"Gmate"} github="" link="https://hackathon-1.azurewebsites.net/" tech={["Javascript","Html","Css","React","cohere ai","azure","flask","node","express"]} description="I have a multipurpose bot which can chat with you and send messeges using gmail,sms,whatsapp"/>
-                <Box name={"Restaurant Menu"} github="https://github.com/rajesh604/restaurant-app" link="https://restaurant-app-c7wi.vercel.app/" tech={["Javascript","Html","Css","React","meal api","hosting"]} description="I have build a restaurant menu"/>
                 <Box name={"Restaurant App"} github="https://github.com/rajesh604/django_fullstack_project" link="http://rajeshkanugu.pythonanywhere.com/" tech={["django","Html","Css","Javascript","meal api","pythonanywhere"]} description="I have build a restaurant app"/>
+                <Box name={"Image Upload"} link="https://improved-funicular.vercel.app/" tech={["Javascript","Html","Css","React","firebase","hosting","mongodb atlas","supabase"]} description="I have build a image upload app"/>
+                <Box name={"Restaurant Menu"} github="https://github.com/rajesh604/restaurant-app" link="https://restaurant-app-c7wi.vercel.app/" tech={["Javascript","Html","Css","React","meal api","hosting"]} description="I have build a restaurant menu"/>
                 <Box name={"Space Exploration"} github="https://github.com/rajesh604/space-tourism" link="https://space-tourism-l7gb.vercel.app/" tech={["Javascript","Html","Css"]} description="I have build a space exploration app"/>
                 <Box name={"Tennis Game"} github="https://github.com/rajesh604/tennis_using_react" link="https://tennis-using-react-nmpk.vercel.app/" tech={["Javascript","Html","Css","React"]} description="I have build a tennis game"/>
             </div>
