b2143b57e72594db176d5b16b2f5036e14f85508
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-21 18:00:29 +0530

    adding all the data

--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -5,7 +5,7 @@ function Section6() {
         <section id="section6">
             <h1><span>Ø5.</span>&lt;get in touch&gt;</h1>
             <p>My inbox is always open.Whether you have a question or project or just want to say hi, I'll try my best to get back to you!</p>
-            <a href="https://ik.imagekit.io/hbzknb1hm/my_resume.pdf?updatedAt=1689769131206" download="rajesh-resume.pdf">My CV</a>
+            <a href="https://ik.imagekit.io/hbzknb1hm/rajeshkanugu.pdf?updatedAt=1689942573533" download="rajesh-resume.pdf">My CV</a>
             <h3>&lt;/get in touch&gt;</h3>
         </section>
     )
