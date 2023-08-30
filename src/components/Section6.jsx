6b3b6319d928a2ea8581ba74f4306e2f405a897f
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-31 01:13:14 +0530

    add

--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -5,7 +5,7 @@ function Section6() {
         <section id="section6">
             <h1><span>Ø5.</span>&lt;get in touch&gt;</h1>
             <p>My inbox is always open.Whether you have a question or project or just want to say hi, I'll try my best to get back to you!</p>
-            <a href="https://ik.imagekit.io/hbzknb1hm/Rajesh_Resume.docx?updatedAt=1692182420291" download="rajesh_resume.docx">My CV</a>
+            <a href="https://ik.imagekit.io/hbzknb1hm/Resume%20(2)%20(2).docx?updatedAt=1693424543283" download="rajesh_resume.docx">My CV</a>
             <h3>&lt;/get in touch&gt;</h3>
         </section>
     )
