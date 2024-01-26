f7e19a26453547a34cd3949316f1c43f924d430d
kanugurajesh <kanugurajesh3@gmail.com>
2024-01-26 07:35:41 +0530

    updating resume link

--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -5,7 +5,7 @@ function Section6() {
         <section id="section6">
             <h1><span>Ø5.</span>&lt;get in touch&gt;</h1>
             <p>My inbox is always open.Whether you have a question or project or just want to say hi, I'll try my best to get back to you!</p>
-            <a href="https://ik.imagekit.io/hbzknb1hm/Resume%20(2)%20(2).docx?updatedAt=1693424543283" download="rajesh_resume.docx">My CV</a>
+            <a href="https://bit.ly/kanugu_rajesh_resume" download="rajesh_resume.docx">My Resume</a>
             <h3>&lt;/get in touch&gt;</h3>
         </section>
     )
