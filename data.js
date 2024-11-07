97b303b7f5ac61cde2a03fbc6acf048904111b25
kanugurajesh <kanugurajesh3@gmail.com>
2024-11-07 20:24:43 +0530

    Adding the updated portfolio

--- data.js
+++ data.js
@@ -1,5 +1,36 @@
 const data = [
     {
+        name: "SoulCare",
+        github: "https://github.com/kanugurajesh/SoulCare",
+        link: "",
+        description: "SoulCare is a mental health app using NLP to analyze social media sentiment, track symptoms, and offer AI-driven support with personalized reports, document uploads, and symptom-based prioritization.",
+        tech: [
+            "Next.js",
+            "Typescript",
+            "Tailwindcss",
+            "Python",
+            "Fast API",
+            "Postgres",
+            "Langchain",
+            "Cohere"
+        ]
+    },
+    {
+        name: "LLM-Chat",
+        github: "https://github.com/kanugurajesh/LLM-Chat",
+        link: "A RAG application to chat with your documents with complete control over your data you can delete your files in the db and see which files are loaded into the vector database and also delete the vectors of a particular file",
+        tech: [
+            "Typescript",
+            "React.Js",
+            "Tailwindcss",
+            "Cohere",
+            "Postgres",
+            "Python",
+            "Fast API",
+            "Langchain"
+        ]
+    },
+    {
         name: "SmartDoc",
         github: "https://github.com/kanugurajesh/SmartDoc",
         link: "https://smart-doc-pearl.vercel.app/",
